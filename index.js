d8d455be7fc29fbdbfde873f12a4f2461a2e43a7
Gavin Anderson <itsbigchamp@Gavins-MacBook-Air.local>
2022-07-21 16:51:33 -0400

    MongoDB

---
+++ index.js
@@ -0,0 +1,24 @@
+  // import mongo tools / library
+const {MongoClient, ServerApiVersion} = require('mongodb');
+
+const {uri} = require('./dbsecrets')
+// connect to Mongo/Atlas USING a URl connection string
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1});
+
+//connect to sample_mflix database
+const db = client.db('sample_mflix');
+
+//Query
+
+// let's get a specific movie (doc) from the 'movies' collection
+// search by title for "Back to the Future"
+db.collection('movies').findOne({title: "Back to the Future"}, (err, results) => {
+    if(err) {
+        console.error(err);
+    }
+  //let's output the results
+  console.log(results); 
+  client.close();
+});
+
